bdea0eea7e370b71563adffcfd9e1aea665b69db
miraedbswo <miraedbswo@gmail.com>
2019-07-14 16:56:15 +0900

    Update: use mysql

--- config/db_config.py
+++ config/db_config.py
@@ -1,18 +1,21 @@
+import os
+
+
 class LocalDBConfig:
     DB_SETTING = {
         'database': 'test',
         'user': 'test',
         'password': 'test',
         'host': '127.0.0.1',
-        'port': 3306
+        'port': 3306,
     }
 
 
 class RemoteDBConfig:
     DB_SETTING = {
-        'database': 'test',
-        'user': 'test',
-        'password': 'test',
-        'host': '127.0.0.1',
-        'port': 3306
+        'database': os.getenv('DB_DATABASE'),
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST', 'dsm-festival.c8yvg2qxmek7.ap-northeast-2.rds.amazonaws.com'),
+        'port': os.getenv('PORT'),
     }
