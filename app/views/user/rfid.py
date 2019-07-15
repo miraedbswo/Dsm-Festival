4184c6178b4bb8343fabf3c6c7a72fb4bb7bbefe
miraedbswo <miraedbswo@gmail.com>
2019-07-15 20:14:55 +0900

    DELETE: pydantic extension

--- app/views/user/rfid.py
+++ app/views/user/rfid.py
@@ -1,13 +1,9 @@
-from pydantic import BaseModel
+from flask import jsonify
 
 from app.decorators.json_schema import json_type_validate, RFID_POST_JSON
 from app.views.base import BaseResource
-from app.models import UnsignedRFIDTable
+from app.models import UnsignedRFIDTable, RFIDTable
 from app.context import context_property
-
-
-class RFIDModel(BaseModel):
-    rfid: str
 
 
 class CreateRFID(BaseResource):
