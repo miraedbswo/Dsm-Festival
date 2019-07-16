099dd0221c287b14ba6b91003539905024812072
miraedbswo <miraedbswo@gmail.com>
2019-07-16 20:14:15 +0900

    ADD: top 10 Rank api

--- app/views/user/rank.py
+++ app/views/user/rank.py
@@ -6,23 +6,9 @@ from app.models import RFIDTable
 
 
 class Rank(BaseResource):
-    @jwt_required
     def get(self):
         top_10_rank = RFIDTable.get_top_10_rank()
-        my_rank = RFIDTable.get_my_rank(get_jwt_identity())
 
-        result = {
-            'top_10_rank': [
-                {
-                    "name": user.name,
-                    "point": user.point,
-                } for user in top_10_rank
-            ],
-
-            'my_rank': {
-                "name": my_rank.name,
-                "point": my_rank.point,
-            },
-        }
-
-        return jsonify(result), 201
+        return {
+            'top_10_rank': top_10_rank
+        }, 201
