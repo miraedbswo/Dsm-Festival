8d30e6239469bd3cf462d616dbdf98bdbb3a6739
Sangmin In <python@istruly.sexy>
2019-07-16 09:29:15 +0900

    GetInfoByRFID url parameter가 잘못된 것 수정, account blueprint에 url prefix 추가

--- app/views/user/__init__.py
+++ app/views/user/__init__.py
@@ -3,11 +3,15 @@ from flask_restful import Api
 
 from app.views.user import info, rfid, login, rank
 
-user_blueprint = Blueprint('user', __name__)
+user_blueprint = Blueprint(
+    name='user',
+    import_name=__name__,
+    url_prefix='/account',
+)
 user_api = Api(user_blueprint)
 
 user_api.add_resource(rfid.CreateRFID, '/rfid')
 user_api.add_resource(info.GetInfoByToken, '/info')
-user_api.add_resource(info.GetInfoByRFID, '/info/{rfid}')
+user_api.add_resource(info.GetInfoByRFID, '/info/<rfid>')
 user_api.add_resource(login.Login, '/login')
 user_api.add_resource(rank.Rank, '/rank')
