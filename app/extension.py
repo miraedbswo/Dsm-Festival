38d8b5502bd77a0d4bfa0794d14315201337d45c
miraedbswo <miraedbswo@gmail.com>
2019-07-18 01:59:30 +0900

    finish

--- app/extension.py
+++ app/extension.py
@@ -8,8 +8,8 @@ db: MySQLDatabase = MySQLDatabase(**{
         'database': os.getenv('DB_DATABASE'),
         'user': os.getenv('DB_USER'),
         'password': os.getenv('DB_PASSWORD'),
-        'host': os.getenv('DB_HOST', 'festival.c8yvg2qxmek7.ap-northeast-2.rds.amazonaws.com'),
-        'port': os.getenv('PORT', 3306),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('PORT'),
     })
 
 jwt = JWTManager()
