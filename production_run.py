59c8a30009ec338fa56bfe84ea9fc3279275c491
miraedbswo <miraedbswo@gmail.com>
2019-07-17 17:23:24 +0900

    ADD: app.run in production_run

--- production_run.py
+++ production_run.py
@@ -8,3 +8,6 @@ if "SECRET_KEY" not in os.environ:
     raise Warning("The secret key must bpipe passed by the <SECRET_KEY> envvar.")
 
 application = create_app(ProductionConfig, RemoteDBConfig)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=5000, threaded=True)
